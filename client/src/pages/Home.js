856b1765fa48d5c0eb88b906f4eb1fc18329416a
mikeydgithub <ampgzo2@gmail.com>
2022-05-19 18:16:18 -0400

    categorymenu and product list to include useEffect

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 
 const Home = () => {
-  // currentCategory is passed to the ProductList component as a prop and instructs which categorys products should be retrieved using Apollo.
-  const [currentCategory, setCategory] = useState("");
-
   return (
     <div className="container">
-      {/*setCatgeory is set a value */}
-      <CategoryMenu setCategory={setCategory} />
-      <ProductList currentCategory={currentCategory} />
+      <CategoryMenu />
+      <ProductList />
     </div>
   );
 };
