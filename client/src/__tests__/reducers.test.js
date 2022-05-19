f412bf2727416deb66e8c5ecc10a74135804f2e4
mikeydgithub <ampgzo2@gmail.com>
2022-05-19 13:46:45 -0400

    test for updat_products with reducer. bring a states length from 2 to 0

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -6,12 +6,17 @@ import {
     UPDATE_CURRENT_CATEGORY
 } from '../utils/actions'
 
+import { reducer } from '../utils/reducers';
+
+
+
 // create a sample of what our global state will look like
 const initialState = {
     products: [],
     categories: [{ name: 'Food' }],
     currentCategory: '1'
 }
+
 
 // update product test
 test('UPDATE_PRODUCTS', () => {
