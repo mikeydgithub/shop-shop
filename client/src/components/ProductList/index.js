d331d30264193d06ea30fb1473601c202aec029a
Mike <ampgzo2@gmail.com>
2022-05-25 12:08:19 -0400

    create indexeddb else if statements for idbpromise to get and put for productlist and categorymenu

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -5,6 +5,7 @@ import ProductItem from '../ProductItem';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
@@ -15,13 +16,30 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
+    // if there's data to be stored
     if (data) {
+      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+
+      // but let's also take each product and save it to the IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product)
+      });
+      // add else if to check if 'loading` is undefined in 'useQuery()` Hook
+    } else if (!loading) {
+      //since we're offline, get all of the data from the 'products' store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global set for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { idbPromise } from '../utils/helpers';
+import { QUERY_PRODUCTS } from '../utils/queries';
 import Cart from '../components/Cart';
 import { useStoreContext } from '../utils/GlobalState';
 import { 
@@ -9,10 +10,7 @@ import {
     UPDATE_CART_QUANTITY,
     ADD_TO_CART,
     UPDATE_PRODUCTS, 
-  } 
-from '../utils/actions';
-
-import { QUERY_PRODUCTS } from '../utils/queries';
+  } from '../utils/actions';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
@@ -50,15 +48,31 @@ function Detail() {
   };
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
       setCurrentProduct(products.find((product) => product._id === id));
-    } else if (data) {
+    }
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
+      });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
       });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts,
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
